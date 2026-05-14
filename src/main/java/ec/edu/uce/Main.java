566a78f98b724df415748b71458c8cef9a4e3cdd
ivonnesandovalin013-web <ivonnesandovalin013@gmail.com>
2026-05-13 19:35:50 -0500

    Taller13

---
+++ src/main/java/ec/edu/uce/Main.java
@@ -0,0 +1,20 @@
+package ec.edu.uce;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Main {
+    public static void main(String[] args) {
+        Quarkus.run(App.class, args);
+    }
+
+    public static class App implements QuarkusApplication {
+        public int run(String... args) {
+            System.out.println("Conexion a base de datos");
+            return 0;
+        }
+    }
+
+}
