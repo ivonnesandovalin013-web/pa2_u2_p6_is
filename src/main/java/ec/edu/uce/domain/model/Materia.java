8b645b70bb9cd5ea808aff9244fa06f7e6a51cd5
ivonnesandovalin013-web <ivonnesandovalin013@gmail.com>
2026-06-17 19:53:41 -0500

    Taller 23: Colecciones @ManyToMany

---
+++ src/main/java/ec/edu/uce/domain/model/Materia.java
@@ -0,0 +1,46 @@
+package ec.edu.uce.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "materia")
+public class Materia {
+
+    @Id
+    @SequenceGenerator(name = "seq_materia_generator", sequenceName = "seq_materia", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_materia_generator")
+    @Column(name = "mate_id")
+    private Integer id;
+    @Column(name = "mate_nombre")
+    private String nombre;
+    @Column(name = "mate_numero_creditos")
+    private Integer numeroCreditos;
+
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Integer getNumeroCreditos() {
+        return numeroCreditos;
+    }
+    public void setNumeroCreditos(Integer numeroCreditos) {
+        this.numeroCreditos = numeroCreditos;
+    }
+    
+
+}
